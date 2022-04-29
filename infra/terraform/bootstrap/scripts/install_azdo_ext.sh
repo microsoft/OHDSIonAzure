0dbc11564e3083439e9356220a404f26e4eb442c
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2022-04-28 17:27:42 -0700

    resolve lint errors

--- infra/terraform/bootstrap/scripts/install_azdo_ext.sh
+++ infra/terraform/bootstrap/scripts/install_azdo_ext.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-az devops extension list | grep "charleszipp.azure-pipelines-tasks-terraform"
-
-if [ $? -eq 0 ]; then
+if az devops extension list | grep "charleszipp.azure-pipelines-tasks-terraform"; then
    echo "Terraform extension exists."
 else
    echo "Terraform extension does not exists. Attempting to install..."
