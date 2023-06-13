56d4828e51e17d44eaab879e82e4fef42a8b1887
anatbal <anatbal@example.com>
2023-06-13 11:19:03 +0300

    remove v1

--- v1/sql/cdm/v5.3.1/omop_vocabulary_ddl/dbo/Tables/dose_era.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[dose_era] (
-    [dose_era_id]         INT        NOT NULL,
-    [person_id]           INT        NOT NULL,
-    [drug_concept_id]     INT        NOT NULL,
-    [unit_concept_id]     INT        NOT NULL,
-    [dose_value]          FLOAT (53) NOT NULL,
-    [dose_era_start_date] DATE       NOT NULL,
-    [dose_era_end_date]   DATE       NOT NULL
-);
--- v1/sql/cdm/v5.3.1/omop_vocabulary_ddl/dbo/Tables/metadata.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [dbo].[metadata] (
-    [metadata_concept_id]      INT           NOT NULL,
-    [metadata_type_concept_id] INT           NOT NULL,
-    [name]                     VARCHAR (250) NOT NULL,
-    [value_as_string]          VARCHAR (MAX) NULL,
-    [value_as_concept_id]      INT           NULL,
-    [metadata_date]            DATE          NULL,
-    [metadata_datetime]        DATETIME2 (7) NULL
-);
